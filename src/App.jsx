3b468fc133ad27d5134bb12605283e9d2f6ee09e
Nik00NN <birziloiu.mihaialexandru@outlook.com>
2024-10-27 13:39:41 +0200

    Added some styles and colors adapted from tailwindcss and imported two fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,41 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: var(--border-radius-sm);
+  background-color: var(--color-brand-600);
+  color: var(--color-brand-50);
+  box-shadow: var(--shadow-sm);
+`;
+
+const Input = styled.input`
+  border: 2px solid var(--color-brand-200);
+  border-radius: 10px;
+  padding: 1rem 1.2rem;
+`;
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <div>
+        <H1>The Wild Oasis</H1>
+        <Button onClick={() => alert("Check In")}>Check In</Button>
+        <Button onClick={() => alert("Check Out")}>Check Out</Button>
+        <Input type="text" placeholder="Enter something" />
+      </div>
+    </>
+  );
 };
 
 export default App;
