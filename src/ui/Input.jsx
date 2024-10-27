726f6cc3acb27ef3ba26fcb91ad74b45f8bfa94b
Nik00NN <birziloiu.mihaialexandru@outlook.com>
2024-10-27 17:05:21 +0200

    Created useMoveBack custom hook, some reusable components and a PageNotFound page

---
+++ src/ui/Input.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const Input = styled.input`
+  border: 2px solid var(--color-brand-200);
+  border-radius: 10px;
+  padding: 1rem 1.2rem;
+`;
+
+export default Input;
